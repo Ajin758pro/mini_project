2d75f87091f403501e88bafd5e55ed0878c85193
Ajin Jayan <ajinjayan758@gmail.com>
2023-03-18 19:05:57 +0530

    update

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class PaymentComponent {
 
+   name:string=''
+   phone:string=''
+   address:string=''
+  // msg()
+  // {
+  //   if(this.name == ''){
+  //     Swal.fire({
+  //       title: 'Please Enter ',
+  //       icon: 'warning',
+  //       confirmButtonColor: '#5a3838',
+  //     })
+  //      return;
+  //   }
+  //   if(this.phone == ''){
+  //     Swal.fire({
+  //       title: 'Please Enter Phone number',
+  //       icon: 'warning',
+  //       confirmButtonColor: '#5a3838',
+  //     })
+  //      return;
+  //   }
+  //    if(this.address=='')
+  //   {
+  //     Swal.fire({
+  //       title: 'Please Enter Address',
+  //       icon: 'warning',
+  //       confirmButtonColor: '#5a3838',
+  //     })
+  //      return
+  //   }
+  //   this.name = '';
+  // this.phone = '';
+   
+  // }
+
+
+ 
 }
